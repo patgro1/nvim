3b65376849a08e3a336731dd4a3f35dfeef01d07
Patrick Grogan <pgrogan@drw.com>
2021-07-14 12:49:10 -0400

    Change nvim-tree mapping scheme

--- plugin/nvimtree.lua
+++ plugin/nvimtree.lua
@@ -24,18 +24,18 @@ vim.api.nvim_tree_hijack_cursor = 0 --1 by default, when moving cursor in the tr
 vim.api.nvim_tree_icon_padding = ' ' --one space by default, used for rendering the space between the icon and the filename. Use with caution, it could break rendering if you set an empty string depending on your font.
 vim.api.nvim_tree_update_cwd = 0 --1 by default, will update the tree cwd when changing nvim's directory (DirChanged event). Behaves strangely with autochdir set.
 --vim.api.nvim_tree_window_picker_exclude = {
---    \   'filetype': [
---    \     'packer',
---    \     'qf'
---    \   ],
---    \   'buftype': [
---    \     'terminal'
---    \   ]
---    \ }
---" Dictionary of buffer option names mapped to a list of option values that
---" indicates to the window picker that the buffer's window should not be
---" selectable.
---let g:nvim_tree_special_files = [ 'README.md', 'Makefile', 'MAKEFILE' ] " List of filenames that gets highlighted with NvimTreeSpecialFile
+    --    \   'filetype': [
+    --    \     'packer',
+    --    \     'qf'
+    --    \   ],
+    --    \   'buftype': [
+    --    \     'terminal'
+    --    \   ]
+    --    \ }
+    --" Dictionary of buffer option names mapped to a list of option values that
+    --" indicates to the window picker that the buffer's window should not be
+    --" selectable.
+    --let g:nvim_tree_special_files = [ 'README.md', 'Makefile', 'MAKEFILE' ] " List of filenames that gets highlighted with NvimTreeSpecialFile
 vim.api.nvim_tree_show_icons = {
     git = 1,
     folders = 0,
@@ -43,17 +43,17 @@ vim.api.nvim_tree_show_icons = {
     folder_arrows = 1
 }
 --let g:nvim_tree_show_icons = {
---    \ 'git': 1,
---    \ 'folders': 0,
---    \ 'files': 0,
---    \ 'folder_arrows': 0,
---    \ }
---"If 0, do not show the icons for one of 'git' 'folder' and 'files'
---"1 by default, notice that if 'files' is 1, it will only display
---"if nvim-web-devicons is installed and on your runtimepath.
---"if folder is 1, you can also tell folder_arrows 1 to show small arrows next to the folder icons.
---"but this will not work when you set indent_markers (because of UI conflict)
---
+    --    \ 'git': 1,
+    --    \ 'folders': 0,
+    --    \ 'files': 0,
+    --    \ 'folder_arrows': 0,
+    --    \ }
+    --"If 0, do not show the icons for one of 'git' 'folder' and 'files'
+    --"1 by default, notice that if 'files' is 1, it will only display
+    --"if nvim-web-devicons is installed and on your runtimepath.
+    --"if folder is 1, you can also tell folder_arrows 1 to show small arrows next to the folder icons.
+    --"but this will not work when you set indent_markers (because of UI conflict)
+    --
 vim.api.nvim_tree_icons = {
     default = '',
     symlink = '',
@@ -85,39 +85,37 @@ vim.api.nvim_tree_icons = {
 }
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 vim.g.nvim_tree_bindings = {
-    -- ["<CR>"] = ":YourVimFunction()<cr>",
-    -- ["u"] = ":lua require'some_module'.some_function()<cr>",
-
-    -- default mappings
-    ["<CR>"]           = tree_cb("edit"),
-    ["o"]              = tree_cb("edit"),
-    ["l"]              = tree_cb("edit"),
-    ["<2-LeftMouse>"]  = tree_cb("edit"),
-    ["<2-RightMouse>"] = tree_cb("cd"),
-    ["<C-]>"]          = tree_cb("cd"),
-    ["v"]              = tree_cb("vsplit"),
-    ["s"]              = tree_cb("split"),
-    ["<C-t>"]          = tree_cb("tabnew"),
-    ["<"]              = tree_cb("prev_sibling"),
-    [">"]              = tree_cb("next_sibling"),
-    ["<BS>"]           = tree_cb("close_node"),
-    ["h"]              = tree_cb("close_node"),
-    ["<S-CR>"]         = tree_cb("close_node"),
-    ["<Tab>"]          = tree_cb("preview"),
-    ["I"]              = tree_cb("toggle_ignored"),
-    ["H"]              = tree_cb("toggle_dotfiles"),
-    ["R"]              = tree_cb("refresh"),
-    ["a"]              = tree_cb("create"),
-    ["d"]              = tree_cb("remove"),
-    ["r"]              = tree_cb("rename"),
-    ["<C-r>"]          = tree_cb("full_rename"),
-    ["x"]              = tree_cb("cut"),
-    ["c"]              = tree_cb("copy"),
-    ["p"]              = tree_cb("paste"),
-    ["[c"]             = tree_cb("prev_git_item"),
-    ["]c"]             = tree_cb("next_git_item"),
-    ["-"]              = tree_cb("dir_up"),
-    ["q"]              = tree_cb("close"),
+    { key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb("edit") },
+    { key = {"<2-RightMouse>", "<C-]>"},    cb = tree_cb("cd") },
+    { key = "<C-v>",                        cb = tree_cb("vsplit") },
+    { key = "<C-x>",                        cb = tree_cb("split") },
+    { key = "<C-t>",                        cb = tree_cb("tabnew") },
+    { key = "<",                            cb = tree_cb("prev_sibling") },
+    { key = ">",                            cb = tree_cb("next_sibling") },
+    { key = "P",                            cb = tree_cb("parent_node") },
+    { key = "<BS>",                         cb = tree_cb("close_node") },
+    { key = "<S-CR>",                       cb = tree_cb("close_node") },
+    { key = "<Tab>",                        cb = tree_cb("preview") },
+    { key = "K",                            cb = tree_cb("first_sibling") },
+    { key = "J",                            cb = tree_cb("last_sibling") },
+    { key = "I",                            cb = tree_cb("toggle_ignored") },
+    { key = "H",                            cb = tree_cb("toggle_dotfiles") },
+    { key = "R",                            cb = tree_cb("refresh") },
+    { key = "a",                            cb = tree_cb("create") },
+    { key = "d",                            cb = tree_cb("remove") },
+    { key = "r",                            cb = tree_cb("rename") },
+    { key = "<C-r>",                        cb = tree_cb("full_rename") },
+    { key = "x",                            cb = tree_cb("cut") },
+    { key = "c",                            cb = tree_cb("copy") },
+    { key = "p",                            cb = tree_cb("paste") },
+    { key = "y",                            cb = tree_cb("copy_name") },
+    { key = "Y",                            cb = tree_cb("copy_path") },
+    { key = "gy",                           cb = tree_cb("copy_absolute_path") },
+    { key = "[c",                           cb = tree_cb("prev_git_item") },
+    { key = "]c",                           cb = tree_cb("next_git_item") },
+    { key = "-",                            cb = tree_cb("dir_up") },
+    { key = "q",                            cb = tree_cb("close") },
+    { key = "g?",                           cb = tree_cb("toggle_help") },
 }
 vim.api.nvim_set_keymap('n', '<Leader>tt', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Leader>tr', ':NvimTreeRefresh<CR>', { noremap = true, silent = true })
