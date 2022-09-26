0e823983ece55a6a13e078ecc24b4849e773903e
Patrick Grogan <po.grogan@gmail.com>
2022-09-26 10:26:18 -0400

    add plugins and impatient

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
+local fn = vim.fn
+local impatient_install_path = fn.stdpath('data') .. '/site/pack/packer/start/impatient.nvim'
+if fn.empty(fn.glob(impatient_install_path)) > 0 then
+    print "Impatient not present, skipping"
+else
+    require("impatient")
+end
+
 require("pat.options")
 require("pat.keymaps")
+require("pat.plugins")
