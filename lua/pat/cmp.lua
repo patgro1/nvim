c852ba05d09545861d044f5c46b784972edfc442
Patrick Grogan <po.grogan@gmail.com>
2022-09-26 21:12:41 -0400

    Fix mapping not applied in nvim-cmp

--- lua/pat/cmp.lua
+++ lua/pat/cmp.lua
@@ -1,18 +1,16 @@
 local lspkind = require'lspkind'
 local cmp = require'cmp'
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
 
-
-cmp.setup {
+local select_opts = {behavior = cmp.SelectBehavior.Select}
+cmp.setup ({
     formatting = {
         format = lspkind.cmp_format({
-            mode = 'symbol_text',
-            menu = {
-                buffer = "[buf]",
-                lsp = "[LSP]",
-                nvim_lua = "[api]",
-                path = "[path]",
-            },
+            mode = "symbol_text"
         }),
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
     },
     sources = {
         { name = 'nvim_lua' },
@@ -20,11 +18,14 @@ cmp.setup {
         { name = 'buffer' },
         { name = 'path' },
     },
-    mappings = cmp.mapping.preset.insert({
+    mapping = cmp.mapping.preset.insert({
+        ["<C-p>"] = cmp.mapping.select_prev_item(select_opts),
+        ["<C-n>"] = cmp.mapping.select_next_item(select_opts),
+        ["<Up>"] = cmp.mapping.select_prev_item(select_opts),
+        ["<Down>"] = cmp.mapping.select_next_item(select_opts),
         ["<C-b>"] =  cmp.mapping.scroll_docs(-4),
         ["<C-f>"] =  cmp.mapping.scroll_docs(4),
         ["<C-space>"] =  cmp.mapping.complete(),
         ["<CR>"] =  cmp.mapping.confirm({ select=true }), -- When nothing is selected, take first item
     }),
-}
-
+})
