85d4906d7fb9cf17136f2e261ec437b169157dd8
Patrick Grogan <po.grogan@gmail.com>
2022-10-31 10:35:27 -0400

    Switch diagnostic to float window

--- lua/pat/lsp/lspconfig.lua
+++ lua/pat/lsp/lspconfig.lua
@@ -5,6 +5,8 @@ local on_attach = function(client, buffer)
     vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover)
+    vim.keymap.set('n', '<leader>n', vim.diagnostic.goto_next)
+    vim.keymap.set('n', '<leader>p', vim.diagnostic.goto_prev)
     vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename)
 end
 
@@ -26,6 +28,19 @@ require('lspconfig').pylsp.setup({
 require('lspconfig').rust_analyzer.setup({
     on_attach = on_attach
 })
+
+-- Setting the diagnostic in floating
+vim.diagnostic.config({
+    virtual_text=false
+})
+local diag_grp = vim.api.nvim_create_augroup("DiagGrp", {clear=true})
+vim.api.nvim_create_autocmd("CursorHold", {
+    group = diag_grp,
+    pattern = "*",
+    command = "lua vim.diagnostic.open_float()"
+    }
+)
+vim.o.updatetime=500
 
 -- Some autocmd used with LSP
 local rust_grp = vim.api.nvim_create_augroup("RustAuto", {clear=true})
