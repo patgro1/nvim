5fd8913dc14c378b2249eede8fbba4b5230096a0
Patrick Grogan <po.grogan@gmail.com>
2023-02-03 10:32:24 -0500

    Fix color mismatch in lualine

--- lua/utils/lualine.lua
+++ lua/utils/lualine.lua
@@ -64,7 +64,7 @@ ins_left {
   function()
     return '▊'
   end,
-  -- color = { fg = colors.blue }, -- Sets highlighting of component
+  color = { fg = colors.blue }, -- Sets highlighting of component
   padding = { left = 0, right = 1 }, -- We don't need space before this
 }
 ins_left {
