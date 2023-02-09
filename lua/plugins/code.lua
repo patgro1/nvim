360cb24abad72c638dc2310155e05eabf1ead23c
Patrick Grogan <po.grogan@gmail.com>
2023-02-08 23:30:06 -0500

    Add nvim-surround

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -83,5 +83,37 @@ return {
                 mode='x'
             }
         }
+    },
+    {
+        -- aliases for surround char:
+        --
+        "kylechui/nvim-surround",
+        version = "v1.x.x",
+        event = "BufReadPost",
+        config = function()
+            require'nvim-surround'.setup({
+                keymaps = {
+                    insert = "<C-g>s",
+                    insert_line = "<C-g>S",
+                    normal = "sa",
+                    normal_cur = "yss",
+                    normal_line = "yS",
+                    normal_cur_line = "ySS",
+                    visual = "S",
+                    visual_line = "gS",
+                    delete = "sd",
+                    change = "sr",
+                },
+                -- NOTE: these are the default aliases... its just to keep track of them
+                aliases = {
+                    ["a"] = ">",
+                    ["b"] = ")",
+                    ["B"] = "}",
+                    ["r"] = "]",
+                    ["q"] = { '"', "'", "`" },
+                    ["s"] = { "}", "]", ")", ">", '"', "'", "`" },
+                },
+            })
+        end
     }
 }
