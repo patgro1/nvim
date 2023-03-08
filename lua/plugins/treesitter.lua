256e967e1d1741f696ef3d2d98c0b936311f0bb0
Patrick Grogan <po.grogan@gmail.com>
2023-03-08 11:24:45 -0500

    Use treesitter in python too now

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,18 +4,18 @@ return {
         "nvim-treesitter/nvim-treesitter",
         event = "BufReadPost",
         config = function()
-            local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+            local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
             parser_config.vhdl = {
                 install_info = {
                     url = "~/workspace/tree-sitter-vhdl", -- local path or git repo
-                    files = {"src/parser.c"},
+                    files = { "src/parser.c" },
                     -- optional entries:
                     branch = "main", -- default branch in case of git repo if different from master
                     generate_requires_npm = false, -- if stand-alone parser without npm dependencies
                     requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
                 },
                 filetype = "vhdl", -- if filetype does not match the parser name
-                require 'nvim-treesitter.configs'.setup {
+                require("nvim-treesitter.configs").setup({
                     ensure_installed = {
                         "python",
                         "cpp",
@@ -30,16 +30,16 @@ return {
                     },
                     highlight = {
                         enable = true,
-                        disable = {"python", }
+                        additional_vim_regex_highlighting = { "python" },
                     },
-                    rainbow = {enable = true}
-                }
+                    rainbow = { enable = true },
+                }),
             }
         end,
         build = ":TSUpdate",
         dependencies = {
-            "p00f/nvim-ts-rainbow"
-        }
+            "p00f/nvim-ts-rainbow",
+        },
     },
-    { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" }
+    { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
 }
