47bbd6ed26e26859522d1f6849e61637c6b5dc1a
Patrick Grogan <patrick.grogan@enyx.fr>
2023-10-20 10:56:05 -0400

    Updated rainbow delimiters to new plugin (old was deprecated)

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,16 +1,21 @@
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-	pattern = { "*" },
-	command = [[%s/\s\+$//e]],
+    pattern = { "*" },
+    command = [[%s/\s\+$//e]],
 })
 
 vim.api.nvim_create_augroup("quickfix", { clear = false })
 vim.api.nvim_create_autocmd({ "QuickFixCmdPost" }, {
-	group = "quickfix",
-	pattern = "[^l]*",
-	command = "cwindow",
+    group = "quickfix",
+    pattern = "[^l]*",
+    command = "cwindow",
 })
 vim.api.nvim_create_autocmd({ "QuickFixCmdPost" }, {
-	group = "quickfix",
-	pattern = "l*",
-	command = "lwindow",
+    group = "quickfix",
+    pattern = "l*",
+    command = "lwindow",
+})
+
+vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+    pattern = { "*" },
+    command = [[:TransparentEnable]],
 })
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,19 +7,20 @@ return {
             local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
             parser_config.vhdl = {
                 install_info = {
-                    url = "~/workspace/tree-sitter-vhdl", -- local path or git repo
+                    url = "~/workspace/tree-sitter-vhdl-existing", -- local path or git repo
                     files = { "src/parser.c" },
                     -- optional entries:
-                    branch = "main", -- default branch in case of git repo if different from master
-                    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+                    branch = "main",                        -- default branch in case of git repo if different from master
+                    generate_requires_npm = false,          -- if stand-alone parser without npm dependencies
                     requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
                 },
-                filetype = "vhdl", -- if filetype does not match the parser name
+                filetype = "vhdl",                          -- if filetype does not match the parser name
                 require("nvim-treesitter.configs").setup({
                     ensure_installed = {
                         "python",
                         "cpp",
                         "verilog",
+                        -- "javascript",
                         "lua",
                         "yaml",
                         "bash",
@@ -30,16 +31,39 @@ return {
                     },
                     highlight = {
                         enable = true,
+                        -- disable = { "vhdl" },
                         additional_vim_regex_highlighting = { "python" },
                     },
-                    rainbow = { enable = true },
                 }),
             }
         end,
         build = ":TSUpdate",
-        dependencies = {
-            "p00f/nvim-ts-rainbow",
-        },
+    },
+    {
+        "HiPhish/rainbow-delimiters.nvim",
+        event = "BufReadPost",
+        config = function()
+            local rainbow_delimiters = require('rainbow-delimiters')
+            require 'rainbow-delimiters.setup'.setup({
+                strategy = {
+                    [''] = rainbow_delimiters.strategy['global'],
+                    vim = rainbow_delimiters.strategy['local'],
+                },
+                query = {
+                    [''] = 'rainbow-delimiters',
+                    lua = 'rainbow-blocks'
+                },
+                highlight = {
+                    'RainbowDelimiterRed',
+                    'RainbowDelimiterYellow',
+                    'RainbowDelimiterBlue',
+                    'RainbowDelimiterOrange',
+                    'RainbowDelimiterGreen',
+                    'RainbowDelimiterViolet',
+                    'RainbowDelimiterCyan',
+                },
+            })
+        end
     },
     { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
 }
